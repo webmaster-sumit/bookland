7f563a4eb06201b8067e8b46008cfde852f65dbf
webmaster <coder4066@gmail.com>
2024-09-27 13:04:58 +0530

    mgg

--- adminside/conn.php
+++ adminside/conn.php
@@ -5,7 +5,7 @@ $dbname = 'fasion'; // Name of your database
 $user = 'root'; // Your MySQL username
 $pass = ''; // Your MySQL password
 
-// Create the connection
+// Create the connection    
 $conn = new mysqli($host, $user, $pass, $dbname);
 
 // Check if the connection was successful
@@ -13,4 +13,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
